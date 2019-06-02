6089200255a8eb5c01c278c466f30f3bc512d3bd
MichalGuz <michalguz@gmail.com>
2019-06-02 23:20:20 +0200

    Initialize two variables for testing

--- testing/src/main/java/TestingMain.java
+++ testing/src/main/java/TestingMain.java
@@ -2,8 +2,7 @@ public class TestingMain {
     public static void main(String[] args) {
         Member member = new Member("Mysterious Member", 999);
 
-
-
-
+        String checkName = member.getMemberName();
+        int checknumber = member.getMemberNumber();
     }
 }
