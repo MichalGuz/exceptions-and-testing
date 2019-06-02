4dbc9b2a370a5d69a3e95d244cbcd7a0c6349c05
MichalGuz <michalguz@gmail.com>
2019-06-02 23:57:25 +0200

    Add if condition for executing the test of method add()

--- testing/src/main/java/Calculator/TestingCalculator.java
+++ testing/src/main/java/Calculator/TestingCalculator.java
@@ -4,7 +4,15 @@ public class TestingCalculator {
     public static void main(String[] args) {
         Calculator calculator= new Calculator(5,3);
 
+        //test of method add()
         int resultOfAdding = calculator.getA() + calculator.getB();
+        if(resultOfAdding == calculator.add(calculator.getA(), calculator.getB())){
+            System.out.println("test OK");
+        } else {
+            System.out.println("error!");
+        }
+
         int resultOfSubtracting = calculator.getA() - calculator.getB();
+
     }
 }
