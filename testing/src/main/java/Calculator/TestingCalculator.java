b7ccebea151be35dd92a92cf766035ddaca708b7
MichalGuz <michalguz@gmail.com>
2019-06-03 00:01:54 +0200

    Add comments for test of method add()

--- testing/src/main/java/Calculator/TestingCalculator.java
+++ testing/src/main/java/Calculator/TestingCalculator.java
@@ -5,14 +5,15 @@ public class TestingCalculator {
         Calculator calculator= new Calculator(5,3);
 
         //test of method add()
+        System.out.println("Start the test of method add()");
         int resultOfAdding = calculator.getA() + calculator.getB();
         if(resultOfAdding == calculator.add(calculator.getA(), calculator.getB())){
             System.out.println("test OK");
         } else {
             System.out.println("error!");
         }
+        System.out.println("Test finished");
 
-        int resultOfSubtracting = calculator.getA() - calculator.getB();
 
     }
 }
