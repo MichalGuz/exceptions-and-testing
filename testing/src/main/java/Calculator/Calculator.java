43fe16652b3b8c834714ef65b4ce4f6f9c51d7a1
MichalGuz <michalguz@gmail.com>
2019-06-02 23:49:38 +0200

    Create class for testing the methods of class Calculator

--- testing/src/main/java/Calculator/Calculator.java
+++ testing/src/main/java/Calculator/Calculator.java
@@ -22,7 +22,7 @@ public class Calculator {
     }
 
     public int subtract(int A, int B){
-        return A -B;
+        return A - B;
     }
 
 }
---
+++ testing/src/main/java/Calculator/TestingCalculator.java
@@ -0,0 +1,7 @@
+package Calculator;
+
+public class TestingCalculator {
+    public static void main(String[] args) {
+
+    }
+}
