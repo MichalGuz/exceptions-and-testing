2777d9c3167a474134588830d316985f8f5bbdb0
MichalGuz <michalguz@gmail.com>
2019-06-02 23:53:15 +0200

    Initialize two variables for testing

--- testing/src/main/java/Calculator/TestingCalculator.java
+++ testing/src/main/java/Calculator/TestingCalculator.java
@@ -2,6 +2,9 @@ package Calculator;
 
 public class TestingCalculator {
     public static void main(String[] args) {
+        Calculator calculator= new Calculator(5,3);
 
+        int resultOfAdding = calculator.getA() + calculator.getB();
+        int resultOfSubtracting = calculator.getA() - calculator.getB();
     }
 }
