3b063d2e694dccd830f0354fdf3154cf17301f7a
MichalGuz <michalguz@gmail.com>
2019-06-03 23:01:34 +0200

    Create new object in section Given

--- testing/src/test/java/PlatformTestSuite.java
+++ testing/src/test/java/PlatformTestSuite.java
@@ -1,10 +1,11 @@
+import Member.Member;
 import org.junit.Test;
 
 public class PlatformTestSuite {
     @Test
     public void testCaseMemberName(){
         // Given
-
+        Member member = new Member("Adam", 101);
         // When
 
         // Then
