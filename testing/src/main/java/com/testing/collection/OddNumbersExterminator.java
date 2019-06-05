b0cb50e808003af0bc591fb88830d4c9e74bb9d1
MichalGuz <michalguz@gmail.com>
2019-06-05 09:58:51 +0200

    Create new object of class ArrayList 'numbers' for filling it with random numbers

--- testing/src/main/java/com/testing/collection/OddNumbersExterminator.java
+++ testing/src/main/java/com/testing/collection/OddNumbersExterminator.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class OddNumbersExterminator {
+    ArrayList<Integer>  numbers = new ArrayList<>();
     ArrayList<Integer> evenNumbers;
     public ArrayList exterminate(ArrayList<Integer> numbers){
         Random generator = new Random();
         int a = generator.nextInt(100);
+
         return evenNumbers;
     }
 }
