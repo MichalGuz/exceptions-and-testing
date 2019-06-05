f8793ae5c7bcab00954221d67cd951f815b54c64
MichalGuz <michalguz@gmail.com>
2019-06-05 09:52:42 +0200

    Create object of class Random for generating random numbers (from 0 to 99)

--- testing/src/main/java/com/testing/collection/OddNumbersExterminator.java
+++ testing/src/main/java/com/testing/collection/OddNumbersExterminator.java
@@ -1,10 +1,13 @@
 package com.testing.collection;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class OddNumbersExterminator {
     ArrayList<Integer> evenNumbers;
     public ArrayList exterminate(ArrayList<Integer> numbers){
+        Random generator = new Random();
+        int a = generator.nextInt(100);
         return evenNumbers;
     }
 }
