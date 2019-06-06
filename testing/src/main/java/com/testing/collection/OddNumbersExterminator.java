cd3cd3882f6b22dda53c0189b6e8399c45590f5d
MichalGuz <michalguz@gmail.com>
2019-06-06 22:55:28 +0200

    Add method of printing the current size of collection.  Delete unnecessary line of code

--- testing/src/main/java/com/testing/collection/OddNumbersExterminator.java
+++ testing/src/main/java/com/testing/collection/OddNumbersExterminator.java
@@ -8,16 +8,13 @@ public class OddNumbersExterminator {
     ArrayList<Integer> evenNumbers = new ArrayList<>();
     public ArrayList exterminate(ArrayList<Integer> numbers){
 
-
-
         int x = 0;
         for(int j = 0; j < numbers.size(); j++){
             x = numbers.get(j);
             if(numbers.get(j) % 2 == 0){
                 evenNumbers.add(x);
             }
-//            System.out.println(evenNumbers.size());
-//            x++;
+            System.out.println(evenNumbers.size());
         }
         return evenNumbers;
 //        System.out.println(evenNumbers.size());
