8d1bee399edee57d220cd76607d6ed545ee09c59
MichalGuz <michalguz@gmail.com>
2019-06-06 23:06:47 +0200

    Add method of printing each even number from the collection

--- testing/src/main/java/com/testing/collection/OddNumbersExterminator.java
+++ testing/src/main/java/com/testing/collection/OddNumbersExterminator.java
@@ -1,7 +1,6 @@
 package com.testing.collection;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 public class OddNumbersExterminator {
 
@@ -12,12 +11,14 @@ public class OddNumbersExterminator {
         for(int j = 0; j < numbers.size(); j++){
             x = numbers.get(j);
             if(numbers.get(j) % 2 == 0){
+                System.out.println(x);
                 evenNumbers.add(x);
             }
-            System.out.println(evenNumbers.size());
+
         }
+        System.out.println("The size of list evenNumbers is: "+ evenNumbers.size());
         return evenNumbers;
-//        System.out.println(evenNumbers.size());
+
     }
 
 //    public void check(){
