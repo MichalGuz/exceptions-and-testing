7e2df8752ef111d0186f9996307f93b3ccd52021
MichalGuz <michalguz@gmail.com>
2019-06-06 22:50:24 +0200

    Paste loop for and fill the ArrayList 'numbers' with random numbers in method main()

--- testing/src/main/java/com/testing/collection/ApplicationCollection.java
+++ testing/src/main/java/com/testing/collection/ApplicationCollection.java
@@ -1,10 +1,16 @@
 package com.testing.collection;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ApplicationCollection {
     public static void main(String[] args) {
         ArrayList<Integer> numbers = new ArrayList<>();
+        for(int i = 0; i < 50; i++){
+            Random generator = new Random();
+            Integer a = generator.nextInt(100);
+            numbers.add(a);
+        }
         OddNumbersExterminator elimninator = new OddNumbersExterminator();
         elimninator.exterminate(numbers);
     }
