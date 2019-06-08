daa1f9bd6c794bf4be53b0f4eff0097167c63205
MichalGuz <michalguz@gmail.com>
2019-06-08 23:53:25 +0200

    Initialize variable 'number' for filling the collecion

--- testing/src/test/java/com/testing/collection/CollectionTestSuite.java
+++ testing/src/test/java/com/testing/collection/CollectionTestSuite.java
@@ -23,6 +23,7 @@ public class CollectionTestSuite {
     public void testOddNumbersExterminatorNormalList(){
         // given
         ArrayList<Integer> listOfNumbers = new ArrayList<>();
+        int number = 1;
 
         // when
 
