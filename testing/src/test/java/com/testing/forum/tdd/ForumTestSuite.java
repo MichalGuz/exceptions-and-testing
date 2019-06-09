05d4af486890e55b41099d5e2439df316c5dc989
MichalGuz <michalguz@gmail.com>
2019-06-10 00:35:36 +0200

    Create method for displaying in the console a message before all tests

--- testing/src/test/java/com/testing/forum/tdd/ForumTestSuite.java
+++ testing/src/test/java/com/testing/forum/tdd/ForumTestSuite.java
@@ -1,5 +1,12 @@
 package com.testing.forum.tdd;
 
+import org.junit.BeforeClass;
+
 public class ForumTestSuite {
     private static int testCounter = 0;
+
+    @BeforeClass
+    public static void beforeAllTests(){
+        System.out.println("this is the beginning of tests");
+    }
 }
