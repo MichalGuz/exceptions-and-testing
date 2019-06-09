bef3e70ef2769b96735370bdb5a10c287a631074
MichalGuz <michalguz@gmail.com>
2019-06-09 22:38:21 +0200

    Create equals() method in class ForumPost

--- testing/src/main/java/com/testing/forum/ForumPost.java
+++ testing/src/main/java/com/testing/forum/ForumPost.java
@@ -1,5 +1,7 @@
 package com.testing.forum;
 
+import java.util.Objects;
+
 public class ForumPost {
     public String postBody;
     public String author;
@@ -15,5 +17,14 @@ public class ForumPost {
 
     public String getAuthor() {
         return author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ForumPost forumPost = (ForumPost) o;
+        return getPostBody().equals(forumPost.getPostBody()) &&
+                getAuthor().equals(forumPost.getAuthor());
     }
 }
