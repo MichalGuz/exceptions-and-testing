dbd4e96d92f44ae5ab85877a30bd4425488f3c14
MichalGuz <michalguz@gmail.com>
2019-06-09 22:47:25 +0200

    Override hashCode() method in class ForumComment

--- testing/src/main/java/com/testing/forum/ForumUser.java
+++ testing/src/main/java/com/testing/forum/ForumUser.java
@@ -4,4 +4,10 @@ public class ForumUser {
     private String name;
     private String realName;
 
+    public ForumUser(String name, String realName){
+        // name visible on forum
+        this.name = name;
+        // real name of the user
+        this.realName = realName;
+    }
 }
