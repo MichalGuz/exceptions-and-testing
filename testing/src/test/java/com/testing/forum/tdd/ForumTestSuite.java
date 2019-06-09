4e9feb84885356758f40ea6b62d420352fedd5c0
MichalGuz <michalguz@gmail.com>
2019-06-10 00:37:01 +0200

    Create method for displaying in the console a message about ending all tests

--- testing/src/test/java/com/testing/forum/tdd/ForumTestSuite.java
+++ testing/src/test/java/com/testing/forum/tdd/ForumTestSuite.java
@@ -1,5 +1,6 @@
 package com.testing.forum.tdd;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
 public class ForumTestSuite {
@@ -8,5 +9,10 @@ public class ForumTestSuite {
     @BeforeClass
     public static void beforeAllTests(){
         System.out.println("this is the beginning of tests");
+    }
+
+    @AfterClass
+    public static void afterAllTests(){
+        System.out.println("All tests are finished");
     }
 }
