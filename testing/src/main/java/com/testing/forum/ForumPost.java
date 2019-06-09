cd599daee933410bad443179e8a264c2b041fd03
MichalGuz <michalguz@gmail.com>
2019-06-09 22:31:45 +0200

    Create costructor of class ForumPost

--- testing/src/main/java/com/testing/forum/ForumPost.java
+++ testing/src/main/java/com/testing/forum/ForumPost.java
@@ -3,4 +3,9 @@ package com.testing.forum;
 public class ForumPost {
     public String postBody;
     public String author;
+
+    public ForumPost(String postBody, String author) {
+        this.postBody = postBody;
+        this.author = author;
+    }
 }
