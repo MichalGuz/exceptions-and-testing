72f6e77aabd1863d55fb0e1331409bd5934f0f28
MichalGuz <michalguz@gmail.com>
2019-06-09 22:42:51 +0200

    Create costructor of class ForumComment

--- testing/src/main/java/com/testing/forum/ForumComment.java
+++ testing/src/main/java/com/testing/forum/ForumComment.java
@@ -4,4 +4,10 @@ public class ForumComment {
     public ForumPost forumPost;
     public String commentBody;
     public String author;
+
+    public ForumComment(ForumPost forumPost, String commentBody, String author) {
+        this.forumPost = forumPost;
+        this.commentBody = commentBody;
+        this.author = author;
+    }
 }
