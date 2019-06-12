6d4c54e3c7e09cd778bcb9b1312b3e133fc92d4a
MichalGuz <michalguz@gmail.com>
2019-06-12 22:28:36 +0200

    Declare variable and create constructor of class WeatherForecast

--- testing/src/main/java/com/testing/weather/stub/WeatherForecast.java
+++ testing/src/main/java/com/testing/weather/stub/WeatherForecast.java
@@ -1,4 +1,10 @@
 package com.testing.weather.stub;
 
 public class WeatherForecast {
+    Temperatures temperatures;
+
+    public WeatherForecast(Temperatures temperatures){
+        this.temperatures = temperatures;
+    }
+
 }
