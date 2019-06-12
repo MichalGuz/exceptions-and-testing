8a8262f26e0e7c95e4ef1208cbcee60d8764bfa3
MichalGuz <michalguz@gmail.com>
2019-06-12 22:43:47 +0200

    Create method for test

--- testing/src/test/java/com/testing/weather/stub/WeatherForecastTestSuite.java
+++ testing/src/test/java/com/testing/weather/stub/WeatherForecastTestSuite.java
@@ -1,4 +1,14 @@
 package com.testing.weather.stub;
 
+import org.junit.Test;
+
 public class WeatherForecastTestSuite {
+    @Test
+    public void testCalculateForecastWithStub(){
+        // given
+
+        // when
+
+        // then
+    }
 }
