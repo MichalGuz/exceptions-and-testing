305d25a9fbdd1a94f8e4d15f32ee2ce2755ad376
MichalGuz <michalguz@gmail.com>
2019-06-12 23:04:38 +0200

    Implements method getTemperatures by adding the dummy data

--- testing/src/main/java/com/testing/weather/stub/TemperaturesStub.java
+++ testing/src/main/java/com/testing/weather/stub/TemperaturesStub.java
@@ -6,6 +6,14 @@ public class TemperaturesStub implements Temperatures{
     @Override
     public HashMap<Integer, Double> getTemperatures() {
         HashMap<Integer, Double> stubResult = new HashMap<Integer, Double>();
+
+        // so-called dummy data
+        stubResult.put(0, 20.5);
+        stubResult.put(1, 24.5);
+        stubResult.put(2, 21.5);
+        stubResult.put(3, 23.0);
+        stubResult.put(4, 25.5);
+
         return  stubResult;
     }
 }
