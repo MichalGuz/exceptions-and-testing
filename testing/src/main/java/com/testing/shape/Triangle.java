6334eb801afe05f13dd3900f5debd791ec8b0bf4
MichalGuz <michalguz@gmail.com>
2019-06-13 23:42:27 +0200

    Create two variables and implements methods in class Triangle

--- testing/src/main/java/com/testing/shape/Triangle.java
+++ testing/src/main/java/com/testing/shape/Triangle.java
@@ -1,5 +1,13 @@
 package com.testing.shape;
 
 public class Triangle implements Shape {
+    public String name;
+    public double area;
+    public String nameOfFigure(){
+        return name;
+    }
 
+    public double areaOfFigure(){
+        return area;
+    }
 }
