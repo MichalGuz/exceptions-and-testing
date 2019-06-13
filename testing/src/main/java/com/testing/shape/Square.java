0322adf5c01da89ab30c056c3ea61dea53903a3f
MichalGuz <michalguz@gmail.com>
2019-06-13 23:40:15 +0200

    Create two variables and implements methods in class Square

--- testing/src/main/java/com/testing/shape/Square.java
+++ testing/src/main/java/com/testing/shape/Square.java
@@ -1,5 +1,13 @@
 package com.testing.shape;
 
 class Square implements Shape{
+    public String name;
+    public double area;
+    public String nameOfFigure(){
+        return name;
+    }
 
+    public double areaOfFigure(){
+        return area;
+    }
 }
