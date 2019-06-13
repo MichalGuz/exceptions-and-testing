715d23488619a204988aa89eb13bb7181f75497f
MichalGuz <michalguz@gmail.com>
2019-06-13 23:43:50 +0200

    Create two variables and implements methods in class Circle

--- testing/src/main/java/com/testing/shape/Circle.java
+++ testing/src/main/java/com/testing/shape/Circle.java
@@ -1,4 +1,13 @@
 package com.testing.shape;
 
 public class Circle implements Shape {
+    public String name;
+    public double area;
+    public String nameOfFigure(){
+        return name;
+    }
+
+    public double areaOfFigure(){
+        return area;
+    }
 }
