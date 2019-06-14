1eb7cec69c8b4491e65f2e34ca7be113ca3fc2ee
MichalGuz <michalguz@gmail.com>
2019-06-14 08:29:29 +0200

    Declare new variable and create constructor of class Circle

--- testing/src/main/java/com/testing/shape/Circle.java
+++ testing/src/main/java/com/testing/shape/Circle.java
@@ -3,6 +3,13 @@ package com.testing.shape;
 public class Circle implements Shape {
     public String name;
     public double area;
+    private double r;
+
+    public Circle(String name, double area, double r) {
+        this.name = name;
+        this.area = area;
+        this.r = r;
+    }
 
     public String nameOfFigure(){
         return name;
