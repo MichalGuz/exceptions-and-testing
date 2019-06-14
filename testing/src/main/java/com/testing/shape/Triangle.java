b7afd6c4597c0f731c4e0285f094f2e779a06272
MichalGuz <michalguz@gmail.com>
2019-06-14 08:34:24 +0200

    Declare new variables and create constructor of class Triangle

--- testing/src/main/java/com/testing/shape/Triangle.java
+++ testing/src/main/java/com/testing/shape/Triangle.java
@@ -3,6 +3,16 @@ package com.testing.shape;
 public class Triangle implements Shape {
     public String name;
     public double area;
+    private double a;
+    private double h;
+
+    public Triangle(String name, double area, double a, double h) {
+        this.name = name;
+        this.area = area;
+        this.a = a;
+        this.h = h;
+    }
+
     public String nameOfFigure(){
         return name;
     }
