c927582de40753a71d535c06c6d7c2a67d1446fe
MichalGuz <michalguz@gmail.com>
2019-06-14 08:30:26 +0200

    Declare new variable and create constructor of class Square

--- testing/src/main/java/com/testing/shape/Square.java
+++ testing/src/main/java/com/testing/shape/Square.java
@@ -3,6 +3,13 @@ package com.testing.shape;
 class Square implements Shape{
     public String name;
     public double area;
+    private double a;
+
+    public Square(String name, double area, double a) {
+        this.name = name;
+        this.area = area;
+        this.a = a;
+    }
 
     public String nameOfFigure(){
         return name;
