767bd8c69b38b5896bd9c917609885f7ac418d5e
MichalGuz <michalguz@gmail.com>
2019-06-16 21:50:15 +0200

    Create new objects of class Square

--- testing/src/test/java/com/testing/shape/SquareTestSuite.java
+++ testing/src/test/java/com/testing/shape/SquareTestSuite.java
@@ -1,4 +1,12 @@
 package com.testing.shape;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SquareTestSuite {
+    @Test
+    public void testGetName() {
+        // given
+        Square square = new Square("square", 2.0);
+    }
 }
