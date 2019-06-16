dd859eeab63323f5e05184734588438e29d0002d
MichalGuz <michalguz@gmail.com>
2019-06-16 19:02:01 +0200

    Create new objects of classes Circle and ShapeCollector

--- testing/src/test/java/com/testing/shape/ShapeCollectorTestSuite.java
+++ testing/src/test/java/com/testing/shape/ShapeCollectorTestSuite.java
@@ -6,7 +6,9 @@ public class ShapeCollectorTestSuite {
     @Test
     public void testAddShape(){
         // given
-        Circle circle = new Circle("circle", )
+        Circle circle = new Circle("circle", 4.0);
+        ShapeCollector shapeCollector = new ShapeCollector();
+
         // when
 
         // then
