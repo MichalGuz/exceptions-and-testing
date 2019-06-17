ccd00151633370e8ecb6f75aa30320ca82b05787
MichalGuz <michalguz@gmail.com>
2019-06-17 23:50:46 +0200

    Create import static mock and the mock of interface Temperatures

--- testing/src/test/java/com/testing/weather/mock/WeatherForecastTestSuite.java
+++ testing/src/test/java/com/testing/weather/mock/WeatherForecastTestSuite.java
@@ -1,7 +1,17 @@
 package com.testing.weather.mock;
 
+import static org.mockito.Mockito.mock;
+import com.testing.weather.stub.Temperatures;
 import org.junit.Test;
 
 public class WeatherForecastTestSuite {
     @Test
+    public void testCalculateForecastwithMock(){
+        // given
+        Temperatures temperaturesMock = mock(Temperatures.class) ;
+
+        // when
+
+        // then
+    }
 }
