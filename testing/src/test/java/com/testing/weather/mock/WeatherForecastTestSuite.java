6889af394fdc5e7c55e61bbf42c1174b4ebb6307
MichalGuz <michalguz@gmail.com>
2019-06-17 23:53:38 +0200

    Create new object Wheatehr

--- testing/src/test/java/com/testing/weather/mock/WeatherForecastTestSuite.java
+++ testing/src/test/java/com/testing/weather/mock/WeatherForecastTestSuite.java
@@ -2,6 +2,7 @@ package com.testing.weather.mock;
 
 import static org.mockito.Mockito.mock;
 import com.testing.weather.stub.Temperatures;
+import com.testing.weather.stub.WeatherForecast;
 import org.junit.Test;
 
 public class WeatherForecastTestSuite {
@@ -9,6 +10,7 @@ public class WeatherForecastTestSuite {
     public void testCalculateForecastwithMock(){
         // given
         Temperatures temperaturesMock = mock(Temperatures.class) ;
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
 
         // when
 
