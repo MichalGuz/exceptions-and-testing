ab2e1c1a82a644ebd7a9bdf60d129b4665d0d20d
MichalGuz <michalguz@gmail.com>
2019-06-18 21:44:22 +0200

    Create methods of interface for counting forum statistics

--- testing/src/main/java/com/testing/forum/statistics/Statistics.java
+++ testing/src/main/java/com/testing/forum/statistics/Statistics.java
@@ -1,4 +1,14 @@
 package com.testing.forum.statistics;
 
+import java.util.List;
+
 public interface Statistics {
+    // list of users names
+    List<String> usersNames();
+
+    // total quantity of forum posts
+    int postsCount();
+
+    // total quntity of forum comments
+    int commentsCount();
 }
