207a4ebe1efc56588087473bb4f036b42a96a038
MichalGuz <michalguz@gmail.com>
2019-06-21 23:19:07 +0200

    Add methods assertEquals() for checking if the test has passed correctly

--- testing/src/test/java/com/testing/forum/statistics/CalculateStatisticsTestSuite.java
+++ testing/src/test/java/com/testing/forum/statistics/CalculateStatisticsTestSuite.java
@@ -152,5 +152,10 @@ public class CalculateStatisticsTestSuite {
         // when
         calculateStatistics.calculateAdvStatistics(statisticsMock);
 
+        // then
+        Assert.assertEquals(0, calculateStatistics.getAverageCommentsPerPost(), 0.001);
+        Assert.assertEquals(0, calculateStatistics.getAverageCommentsPerUser(), 0.001);
+        Assert.assertEquals(0, calculateStatistics.getAveragePostsPerUser(), 0.001);
+
     }
 }
