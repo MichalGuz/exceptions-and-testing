819119a527e43fa5a4ad8f86bba0a50055eee51f
MichalGuz <michalguz@gmail.com>
2019-06-22 22:28:34 +0200

    Create constructor and get methods in class 'Book'

--- testing/src/main/java/com/testing/library/Book.java
+++ testing/src/main/java/com/testing/library/Book.java
@@ -4,4 +4,24 @@ public class Book {
     String title;
     String author;
     int yearOfRelease;
+
+    public Book(String title, String author, int yearOfRelease) {
+        this.title = title;
+        this.author = author;
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+
 }
