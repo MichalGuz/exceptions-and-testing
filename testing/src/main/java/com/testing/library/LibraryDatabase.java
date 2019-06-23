0cacd4f53b526e901f061fe41347b7394f3a6079
MichalGuz <michalguz@gmail.com>
2019-06-24 00:19:46 +0200

    Create body of interface LibraryDatabase with methods to implement

--- testing/src/main/java/com/testing/library/LibraryDatabase.java
+++ testing/src/main/java/com/testing/library/LibraryDatabase.java
@@ -1,4 +1,20 @@
 package com.testing.library;
 
+import java.util.List;
+
 public interface LibraryDatabase {
+    // list books having title beginning with fragment of title
+    List<Book> listBooksWithCondition(String titleFragment);
+
+    // list books borrowed by LibraryUser
+    List<Book> listBooksInHandsOf(LibraryUser libraryUser);
+
+    // try to rent a book for LibraryUser
+    // return true when success
+    // and return false when book is unavailable to rent
+    boolean rentABook(LibraryUser libraryUser, Book book);
+
+    // return all books borrowed by libraryUser to the library
+    // returns number of books returned back
+    int returnedBooks(LibraryUser libraryUser);
 }
