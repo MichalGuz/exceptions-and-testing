68b9e2b14dfc69e13cb6f016797b877b607513cd
MichalGuz <michalguz@gmail.com>
2019-06-23 22:24:36 +0200

    Override equals() and hashCode() methods in class LibraryUser.

--- testing/src/main/java/com/testing/library/LibraryUser.java
+++ testing/src/main/java/com/testing/library/LibraryUser.java
@@ -1,5 +1,7 @@
 package com.testing.library;
 
+import java.util.Objects;
+
 public class LibraryUser {
     String firstName;
     String lastName;
@@ -30,5 +32,23 @@ public class LibraryUser {
                 ", lastName='" + lastName + '\'' +
                 ", peselId='" + peselId + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LibraryUser that = (LibraryUser) o;
+        return Objects.equals(getFirstName(), that.getFirstName()) &&
+                Objects.equals(getLastName(), that.getLastName()) &&
+                Objects.equals(getPeselId(), that.getPeselId());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + peselId.hashCode();
+        return result;
     }
 }
