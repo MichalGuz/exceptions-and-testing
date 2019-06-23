7d6b34002cd62bd2fd49010e20d7e69f415e2568
MichalGuz <michalguz@gmail.com>
2019-06-23 22:20:32 +0200

    Create constructor in class LibraryUser.

--- testing/src/main/java/com/testing/library/LibraryUser.java
+++ testing/src/main/java/com/testing/library/LibraryUser.java
@@ -4,4 +4,10 @@ public class LibraryUser {
     String firstName;
     String lastName;
     String peselId;
+
+    public LibraryUser(String firstName, String lastName, String peselId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.peselId = peselId;
+    }
 }
