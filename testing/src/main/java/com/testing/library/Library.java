ea57a7b2b8013441cc0364d6ea26171a8fd4a402
MichalGuz <michalguz@gmail.com>
2019-06-24 20:02:59 +0200

    Declare a variable and crate constructor in class Library

--- testing/src/main/java/com/testing/library/Library.java
+++ testing/src/main/java/com/testing/library/Library.java
@@ -1,4 +1,9 @@
 package com.testing.library;
 
 public class Library {
+    LibraryDatabase libraryDatabase;
+
+    public Library(LibraryDatabase libraryDatabase) {
+        this.libraryDatabase = libraryDatabase;
+    }
 }
