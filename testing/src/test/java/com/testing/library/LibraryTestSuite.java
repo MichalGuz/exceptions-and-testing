1659dc3b56ad790de5692def58c4270519a55941
MichalGuz <michalguz@gmail.com>
2019-06-24 20:20:48 +0200

    Create method of tests with temporarily body.

--- testing/src/test/java/com/testing/library/LibraryTestSuite.java
+++ testing/src/test/java/com/testing/library/LibraryTestSuite.java
@@ -1,4 +1,22 @@
 package com.testing.library;
 
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
 public class LibraryTestSuite {
+    @Test
+    public void testListOfBooksWithConditionReturnList(){
+        assertTrue(false);
+    }
+
+    @Test
+    public void testListOfBooksWithConditionMoreThan20(){
+        assertTrue(false);
+    }
+
+    @Test
+    public void testListOfBooksWithConditionLessThan3(){
+        assertTrue(false);
+    }
+
 }
