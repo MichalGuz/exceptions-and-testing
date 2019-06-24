3b697507a96b0954a7dba5a710be111757341512
MichalGuz <michalguz@gmail.com>
2019-06-24 20:10:49 +0200

    Create a draft of method listOfBooksWithCondition() which will be useful for searching books by the fragment of title. This class will be implemented later.

--- testing/src/main/java/com/testing/library/Library.java
+++ testing/src/main/java/com/testing/library/Library.java
@@ -1,9 +1,21 @@
 package com.testing.library;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Library {
     LibraryDatabase libraryDatabase;
 
     public Library(LibraryDatabase libraryDatabase) {
         this.libraryDatabase = libraryDatabase;
+    }
+
+    public List<Book> listOfBooksWithCondition(String titleFragment){
+        List<Book> bookList = new ArrayList<>();
+        Book book = new Book("The book title", "The book author", 2010);
+        bookList.add(book);
+
+        // temporary return list of one book
+        return bookList;
     }
 }
