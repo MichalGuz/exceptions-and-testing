0087bf0f3d1cb9f6fcf0ee4bcfddf08f2068a2d2
MichalGuz <michalguz@gmail.com>
2019-08-18 23:52:59 +0200

    Transform String with path description into an obejct of Path class.

--- testing/src/main/java/com/exceptions/io/FileReader.java
+++ testing/src/main/java/com/exceptions/io/FileReader.java
@@ -1,11 +1,14 @@
 package com.exceptions.io;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileReader {
     public void readFile(){
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
+        Path path = Paths.get(file.getPath());
 
         System.out.println(file.getPath());
     }
