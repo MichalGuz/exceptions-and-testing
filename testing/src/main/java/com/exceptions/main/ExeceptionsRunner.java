7b7ed1672956f145bd67826e29e1ac4b8f176281
MichalGuz <michalguz@gmail.com>
2019-08-18 16:38:39 +0200

    Create main method in ExceptionsRunner.

--- testing/src/main/java/com/exceptions/main/ExeceptionsRunner.java
+++ testing/src/main/java/com/exceptions/main/ExeceptionsRunner.java
@@ -1,4 +1,10 @@
 package com.exceptions.main;
 
+import com.exceptions.io.FileReader;
+
 public class ExeceptionsRunner {
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader();
+        fileReader.readFile();
+    }
 }
