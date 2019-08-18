d574021bbdc4abc788edc9539024e69913e796fb
MichalGuz <michalguz@gmail.com>
2019-08-18 23:47:12 +0200

    Print the path of file.

--- testing/src/main/java/com/exceptions/io/FileReader.java
+++ testing/src/main/java/com/exceptions/io/FileReader.java
@@ -6,5 +6,7 @@ public class FileReader {
     public void readFile(){
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
+
+        System.out.println(file.getPath());
     }
 }
