7bbf202ef66e9d726d804d53fbd47c9b3af768bf
MichalGuz <michalguz@gmail.com>
2019-08-18 23:43:39 +0200

    Create object of class File and invoke methodd of access to the text file from resources.

--- testing/src/main/java/com/exceptions/io/FileReader.java
+++ testing/src/main/java/com/exceptions/io/FileReader.java
@@ -1,7 +1,10 @@
 package com.exceptions.io;
 
+import java.io.File;
+
 public class FileReader {
     public void readFile(){
         ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("file/names.txt").getFile());
     }
 }
