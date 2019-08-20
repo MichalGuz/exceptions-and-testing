b81b9c7f069620f4a98a672584425a4715f78ba2
MichalGuz <michalguz@gmail.com>
2019-08-20 20:54:04 +0200

    Create method probablyWillThrowsException() in class ExceptionExercise.

--- testing/src/main/java/com/exceptions/exception_excercise/ExceptionExercise.java
+++ testing/src/main/java/com/exceptions/exception_excercise/ExceptionExercise.java
@@ -1,4 +1,10 @@
 package com.exceptions.exception_excercise;
 
 public class ExceptionExercise {
+    public String probablyWillThrowException(double a, double b) throws Exception {
+        if (a >= 2 ||  a < 1 || b == 1.5) {
+            throw new Exception();
+        }
+        return "Done!";
+    }
 }
