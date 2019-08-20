219fdcfbc572eae1cfbd4460fa6462e30ba8721d
MichalGuz <michalguz@gmail.com>
2019-08-20 20:58:52 +0200

    Create block 'try' in  method main() in class ExceptionExcerciseHandling.

--- testing/src/main/java/com/exceptions/exception_excercise/ExceptionExerciseHandling.java
+++ testing/src/main/java/com/exceptions/exception_excercise/ExceptionExerciseHandling.java
@@ -3,6 +3,8 @@ package com.exceptions.exception_excercise;
 public class ExceptionExerciseHandling {
     public static void main(String[] args) {
         ExceptionExercise exercise = new ExceptionExercise();
-
+        try {
+            exercise.probablyWillThrowException(1, 2);
+        }
     }
 }
