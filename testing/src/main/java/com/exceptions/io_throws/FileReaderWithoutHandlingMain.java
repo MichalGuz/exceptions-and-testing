9670f5dbb122a08fb83100c249020a4c019a4e95
MichalGuz <michalguz@gmail.com>
2019-08-20 20:28:44 +0200

    Remove unnecessary import in class FileReaderWithoutHandlingMain.

--- testing/src/main/java/com/exceptions/io_throws/FileReaderWithoutHandlingMain.java
+++ testing/src/main/java/com/exceptions/io_throws/FileReaderWithoutHandlingMain.java
@@ -1,7 +1,5 @@
 package com.exceptions.io_throws;
 
-import com.exceptions.io.FileReader;
-
 public class FileReaderWithoutHandlingMain {
     public static void main(String[] args) {
         FileReaderWithoutHandling fileReader = new FileReaderWithoutHandling();
