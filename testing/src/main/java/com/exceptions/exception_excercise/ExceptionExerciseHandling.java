404695509d95682014df3ab1535c2eb1dfc6471f
MichalGuz <michalguz@gmail.com>
2019-08-20 20:58:04 +0200

    Create method main() with new object of class ExceptionExercise in class ExceptionExcerciseHandling.

---
+++ testing/src/main/java/com/exceptions/exception_excercise/ExceptionExerciseHandling.java
@@ -0,0 +1,8 @@
+package com.exceptions.exception_excercise;
+
+public class ExceptionExerciseHandling {
+    public static void main(String[] args) {
+        ExceptionExercise exercise = new ExceptionExercise();
+
+    }
+}
