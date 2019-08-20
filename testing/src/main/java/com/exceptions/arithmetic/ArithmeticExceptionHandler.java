1bb010d5baa458e222bd7c26bd50840c68219844
MichalGuz <michalguz@gmail.com>
2019-08-20 19:33:00 +0200

    Create method divide() of class ArithmeticExceptionHandler.

--- testing/src/main/java/com/exceptions/arithmetic/ArithmeticExceptionHandler.java
+++ testing/src/main/java/com/exceptions/arithmetic/ArithmeticExceptionHandler.java
@@ -1,5 +1,10 @@
 package com.exceptions.arithmetic;
 
 public class ArithmeticExceptionHandler {
-
+    public double divide(double a, double b) throws ArithmeticException {
+        if(b == 0){
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
 }
