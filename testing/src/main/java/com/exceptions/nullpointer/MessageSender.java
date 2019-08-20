73758706369c8e72abfb28949598d8f30e3ffcee
MichalGuz <michalguz@gmail.com>
2019-08-20 21:32:08 +0200

    Add if condition in method sendMessageTo().

--- testing/src/main/java/com/exceptions/nullpointer/MessageSender.java
+++ testing/src/main/java/com/exceptions/nullpointer/MessageSender.java
@@ -2,6 +2,8 @@ package com.exceptions.nullpointer;
 
 public class MessageSender {
     public void sendMessageTo(User user, String message) {
+        if (user != null){
         System.out.println("Sending message: " + message + " to: " + user.getName() );
+        }
     }
 }
--- testing/src/main/java/com/exceptions/nullpointer/NullpointerRunner.java
+++ testing/src/main/java/com/exceptions/nullpointer/NullpointerRunner.java
@@ -1,4 +1,10 @@
 package com.exceptions.nullpointer;
 
 public class NullpointerRunner {
+    public static void main(String[] args) {
+        User user = null;
+
+        MessageSender messageSender = new MessageSender();
+        messageSender.sendMessageTo(user, "Hello!");
+    }
 }
