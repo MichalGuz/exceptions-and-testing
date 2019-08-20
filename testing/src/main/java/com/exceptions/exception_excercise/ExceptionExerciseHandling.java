b2c408b41836d7c1dbd3470213ba82509a9ecf3f
MichalGuz <michalguz@gmail.com>
2019-08-20 21:09:25 +0200

    Change value of argument to cause an exception.

--- testing/src/main/java/com/exceptions/exception_excercise/ExceptionExerciseHandling.java
+++ testing/src/main/java/com/exceptions/exception_excercise/ExceptionExerciseHandling.java
@@ -4,7 +4,7 @@ public class ExceptionExerciseHandling {
     public static void main(String[] args) {
         ExceptionExercise exercise = new ExceptionExercise();
         try {
-            exercise.probablyWillThrowException(1, 2);
+            exercise.probablyWillThrowException(2, 2);
         } catch (Exception e){
             System.out.println("Argument's error! Please check values of arguments and try again.");
         } finally {
