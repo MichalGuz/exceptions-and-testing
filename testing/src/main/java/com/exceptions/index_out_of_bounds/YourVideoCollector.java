c9fc0a75de7008a5ffa7ccdf58bf1a4c08716c83
MichalGuz <michalguz@gmail.com>
2019-08-20 21:49:38 +0200

    Create method getColllection() of class YourVideoCollector.

--- testing/src/main/java/com/exceptions/index_out_of_bounds/YourVideoCollector.java
+++ testing/src/main/java/com/exceptions/index_out_of_bounds/YourVideoCollector.java
@@ -1,4 +1,13 @@
 package com.exceptions.index_out_of_bounds;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class YourVideoCollector {
+    public List<String> getCollectio() {
+        ArrayList<String> videos = new ArrayList<>();
+        videos.add("Movie 1");
+        videos.add("Movie 2");
+        return videos;
+    }
 }
